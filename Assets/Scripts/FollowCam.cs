4577805609364c8fcd561b7fce01e3edb374d158
Kelsey Mahar <KM922299@wcupa.edu>
2020-10-19 20:28:58 -0400

    Fourth Commit
    Improving camera

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -6,6 +6,9 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+
     [Header("Set Dynamically")]
     public float camZ;
 
@@ -18,6 +21,8 @@ public class FollowCam : MonoBehaviour
 
         // get pos of the poi
         Vector3 destination = POI.transform.position;
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // Force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         // Set the camera to be the destination
